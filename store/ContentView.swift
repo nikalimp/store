51e90c1ba096a001adad07f6b42f0225544ad5e2
Nikita <nikitaaalimpiev@gmail.com>
2022-06-09 22:46:03 +0500

    First commit

---
+++ store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  store
+//
+//  Created by Никита Алимпиев on 09.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ store/storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  storeApp.swift
+//  store
+//
+//  Created by Никита Алимпиев on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
